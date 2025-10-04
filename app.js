a3d8a547b799164b8626f22f9c5391e9cc850037
Rowhnn <140674192+Rowhnn@users.noreply.github.com>
2025-10-04 13:14:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
 const port = 2323;
-
 const https = require("https");
 
+app.use(cors({ origin: 'https://yourusername.github.io' })); // your GitHub Pages URL
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -13,9 +13,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body.cityName);
-  // console.log("Post request recieved.");
-
   const query = req.body.cityName;
   const apiKey = "03597192f8ca3c481081503063db0979";
   const unit = "metric";
@@ -25,37 +22,89 @@ app.post("/", (req, res) => {
     "&appid=" +
     apiKey +
     "&units=" +
-    unit +
-    "";
+    unit;
 
   https.get(url, (response) => {
-    console.log(response.statusCode);
-
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
-      // console.log(weatherData);
-
       const temp = weatherData.main.temp;
-      console.log(temp);
-
       const desc = weatherData.weather[0].description;
-      console.log(desc);
-
       const icon = weatherData.weather[0].icon;
       const iconURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
       res.write("<p>The weather is currently " + desc + ".</p>");
-      res.write(
-        "<h1>The weather of " + query + " is  " + temp + " degree Celsius.</h1>"
-      );
-      res.write("<img src = " + iconURL + ">");
+      res.write("<h1>The weather of " + query + " is " + temp + " °C.</h1>");
+      res.write("<img src=" + iconURL + ">");
       res.send();
     });
   });
-
-  //   res.send("Server is up and running...");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+  console.log(`Server running at ${port}`);
 });
+
+
+
+// const express = require("express");
+// const app = express();
+
+// const port = 2323;
+
+// const https = require("https");
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.post("/", (req, res) => {
+//   console.log(req.body.cityName);
+//   // console.log("Post request recieved.");
+
+//   const query = req.body.cityName;
+//   const apiKey = "03597192f8ca3c481081503063db0979";
+//   const unit = "metric";
+//   const url =
+//     "https://api.openweathermap.org/data/2.5/weather?q=" +
+//     query +
+//     "&appid=" +
+//     apiKey +
+//     "&units=" +
+//     unit +
+//     "";
+
+//   https.get(url, (response) => {
+//     console.log(response.statusCode);
+
+//     response.on("data", (data) => {
+//       const weatherData = JSON.parse(data);
+//       // console.log(weatherData);
+
+//       const temp = weatherData.main.temp;
+//       console.log(temp);
+
+//       const desc = weatherData.weather[0].description;
+//       console.log(desc);
+
+//       const icon = weatherData.weather[0].icon;
+//       const iconURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+//       res.write("<p>The weather is currently " + desc + ".</p>");
+//       res.write(
+//         "<h1>The weather of " + query + " is  " + temp + " degree Celsius.</h1>"
+//       );
+//       res.write("<img src = " + iconURL + ">");
+//       res.send();
+//     });
+//   });
+
+//   //   res.send("Server is up and running...");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at ${port}`);
+// });
+
